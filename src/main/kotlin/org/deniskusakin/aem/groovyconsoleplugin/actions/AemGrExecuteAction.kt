769372dab342bc6ae225f1bc07047e8d156f38f2
cathcart93 <korzhemanova12>
2018-06-20 23:28:26 +0300

    some updates

---
+++ src/main/kotlin/org/deniskusakin/aem/groovyconsoleplugin/actions/AemGrExecuteAction.kt
@@ -0,0 +1,11 @@
+package org.deniskusakin.aem.groovyconsoleplugin.actions
+
+import com.intellij.icons.AllIcons
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class AemGrExecuteAction : AnAction(AllIcons.Toolwindows.ToolWindowRun) {
+    override fun actionPerformed(e: AnActionEvent?) {
+
+    }
+}
---
+++ src/main/kotlin/org/deniskusakin/aem/groovyconsoleplugin/console/EditorDecorator.kt
@@ -0,0 +1,28 @@
+package org.deniskusakin.aem.groovyconsoleplugin.console
+
+import com.intellij.openapi.actionSystem.ActionManager
+import com.intellij.openapi.actionSystem.DefaultActionGroup
+import com.intellij.openapi.editor.impl.EditorHeaderComponent
+import com.intellij.openapi.fileEditor.FileEditor
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Key
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.ui.EditorNotifications
+import org.deniskusakin.aem.groovyconsoleplugin.actions.AemGrExecuteAction
+import javax.swing.JComponent
+
+class EditorDecorator(private val project: Project) : EditorNotifications.Provider<JComponent>() {
+    companion object {
+        private val myKey = Key.create<JComponent>("aem.groovy.console.toolbar")
+    }
+
+    override fun getKey(): Key<JComponent> = myKey
+
+    override fun createNotificationPanel(file: VirtualFile, fileEditor: FileEditor): JComponent? {
+        val actionGroup = DefaultActionGroup(AemGrExecuteAction())
+        val menu = ActionManager.getInstance().createActionToolbar("AemGroovyConsole", actionGroup, true)
+        return EditorHeaderComponent().apply {
+            add(menu.component)
+        }
+    }
+}
