d350cfd15bc81873cc8349bf3b9e8501cb42a69c
Denis_Kusakin <Denis_Kusakin@epam.com>
2018-06-21 19:32:35 +0300

    some progress

--- src/main/kotlin/org/deniskusakin/aem/groovyconsoleplugin/run/configuration/AemGroovyRunConfiguration.kt
+++ src/main/kotlin/org/deniskusakin/aem/groovyconsoleplugin/run/configuration/AemGroovyRunConfiguration.kt
@@ -2,6 +2,8 @@ package org.deniskusakin.aem.groovyconsoleplugin.run.configuration
 
 import com.intellij.execution.Executor
 import com.intellij.execution.configurations.*
+import com.intellij.execution.process.OSProcessHandler
+import com.intellij.execution.process.ProcessHandler
 import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.openapi.options.SettingsEditor
 import com.intellij.openapi.project.Project
@@ -15,6 +17,23 @@ class AemGroovyRunConfiguration(project: Project, factory: ConfigurationFactory,
     }
 
     override fun getState(executor: Executor, environment: ExecutionEnvironment): RunProfileState? {
-        return null
+        //val commandLine = GeneralCommandLine("curl")
+        return GrState(environment)
     }
+}
+
+class GrState(env: ExecutionEnvironment) : CommandLineState(env){
+    val path = "D:\\Projects\\aem-groovyconsole-idea-plugin\\src\\main\\resources\\t.groovy"
+    val username = "admin"
+    val password = "admin"
+    val str = "curl -u ${username}:${password} --data-urlencode script@${path} -X POST -H \"Content-Type: application/x-www-form-urlencoded; charset=UTF-8\" http://localhost:4502/bin/groovyconsole/post.json"
+    override fun startProcess(): ProcessHandler {
+        val commandLine = GeneralCommandLine(str)
+//                .withParameters("-d", "\"scriptPath=${path}\"", "-X", "POST", "-u", "admin:admin", "http://localhost:4502/bin/groovyconsole/post.json")
+        val processHandler = OSProcessHandler(commandLine)
+        processHandler.setShouldDestroyProcessRecursively(false)
+        //processHandler.startNotify()
+        return processHandler
+    }
+
 }
