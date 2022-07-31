398ec43a2ec99a3a207a2caeb6cae3941ed6d1cf
abardashevskiy <andrey.bardashevskiy@infrontx.com>
2022-07-31 17:33:45 +0300

    fixed urls in output

--- src/main/kotlin/org/deniskusakin/aem/groovyconsoleplugin/console/AEMGroovyConsole.kt
+++ src/main/kotlin/org/deniskusakin/aem/groovyconsoleplugin/console/AEMGroovyConsole.kt
@@ -180,7 +180,7 @@ class AEMGroovyConsole(
                             //This code relies on fact that AEM Groovy Console uses Script1.groovy as file name, so this code is highly dangerous
                             //In some obvious cases it could work incorrectly, but it provides user with better experience
                             view.print(
-                                output.exceptionStackTrace.replace("Script1.groovy", contentFile.url),
+                                output.exceptionStackTrace.replace("Script1.groovy", contentFile.presentableUrl),
                                 ConsoleViewContentType.ERROR_OUTPUT
                             )
                         }
